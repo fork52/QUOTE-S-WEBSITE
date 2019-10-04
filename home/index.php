8de9ebb5f5099d2033aa49cd85586c69c07325ec
fork52 <mrunankmistry52@gmail.com>
2019-10-04 21:32:49 +0530

    JS added

--- home/index.php
+++ home/index.php
@@ -4,12 +4,13 @@
   <title>Quote's Website</title>
 
   <meta name="viewport" content="width=device-width, initial-scale=1">
-  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
-  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
-  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
 
+  <link rel="stylesheet" href="../BS/css/bootstrap.min.css">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+  <script src="../BS/js/jquery.min.js"></script>
+  <script src="../BS/js/bootstrap.min.js"></script>
   <link href="style.css" rel="stylesheet" />
+
   <link href="../loginForm/loginStyle.css" rel="stylesheet" />
 
 </head>
@@ -135,7 +136,6 @@
 <br> <br>
 
 <?php include "../includes/footer.php" ?>
-
 <?php include "../loginForm/loginHome.php" ?>
 
 
--- includes/header_links.php
+++ includes/header_links.php
@@ -1,12 +1,40 @@
 <div class="collapse navbar-collapse" id="navbar-collapse-main">
 	<ul class="nav navbar-nav navbar-right">
-
-		<li><a class="active" href="../home/index.php">HOME </a></li>
-		<li><a  shref="#">PROFILE </a></li>
-		<li><a  href="../AUTHORS/aboutauthors.php">AUTHORS </a></li>
-		<li><a  href="../rgform/regform.php">REGISTER </a></li>
-		<li><a  href="#myModal" data-toggle="modal">LOGIN </a></li>
-		<li><a  href="../aboutus/aboutus.php">ABOUT </a></li>
-
+		
+	
+		<li><a  class="link1" href="../home/index.php">HOME </a></li>
+		<li><a  class="link2" href="#">PROFILE </a></li>
+		<li><a  class="link3" href="../AUTHORS/aboutauthors.php">AUTHORS </a></li>
+		<li><a  class="link4" href="../rgform/regform.php">REGISTER </a></li>
+		<li><a  class="link5" href="#myModal" data-toggle="modal">LOGIN </a></li>
+		<li><a  class="link6" href="../aboutus/aboutus.php">ABOUT </a></li>
 	</ul>
 </div>
+
+<script type="text/javascript">
+	
+	var url = window.location.pathname;
+	var filename = url.substring(url.lastIndexOf('/')+1);
+
+	var header = document.getElementById("navbar-collapse-main");
+	// alert(filename);
+
+	if(filename=="index.php"){
+			var current_link = header.getElementsByClassName("link1");
+			current_link[0].className+=" active";
+	}
+	else if(filename=="regform.php"){
+		var current_link = header.getElementsByClassName("link4");
+		current_link[0].className+=" active";
+	}
+	else if(filename=="aboutus.php"){
+		var current_link = header.getElementsByClassName("link6");
+		current_link[0].className+=" active";
+	}
+
+	var login_link  = header.getElementsByClassName("link5");
+	login_link[0].addEventListener("click", function() {
+		this.className +=" active";
+	});
+
+</script>
