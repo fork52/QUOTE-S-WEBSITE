10c27bc6e6b8592305f2795045e8740090157352
fork52 <mrunankmistry52@gmail.com>
2019-10-05 21:11:40 +0530

    Update and backup

--- home/index.php
+++ home/index.php
@@ -6,7 +6,6 @@
   <meta name="viewport" content="width=device-width, initial-scale=1">
 
   <link rel="stylesheet" href="../BS/css/bootstrap.min.css">
-  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
   <link rel='stylesheet' href='https://fonts.googleapis.com/css?family=Montserrat:300,400,700'>
   <link rel="stylesheet" href="cardStyle.css">
 
@@ -133,64 +132,12 @@
 
 <br> <br>
 
-<div class="container">
-  <div class="cardWrapper">
-      <h1>Quotes</h1>
-      <div class="cardCols">
-
-<?php
-
-      $sql_query = "SELECT * FROM quotes WHERE LENGTH(quote_text<=27) ORDER BY RAND() LIMIT 10" ; 
-      $result = mysqli_query($conn,$sql_query);
-      $resultCheck = mysqli_num_rows($result);
-      $counter = 501;
-
-      if($resultCheck>0){
-        // shuffle_assoc($result);
 
 
-        while ( $row = mysqli_fetch_assoc($result)) {
+ <?php include_once "../includes/home_quoteCards.php" ?>
 
-             
-          $quote =$row["quote_text"];
-          $author_name=$row["quote_author"];
-          $url_img_no = $counter;           //Counter var
-          $counter ++;
-          $url_img_no=(string) $url_img_no;
-          $url_img = "https://unsplash.it/".$url_img_no."/".$url_img_no."/";
-
-          echo "<div class=\"cardColumn\" ontouchstart=\"this.classList.toggle('hover'); \"> ";
-          echo "<div class=\"cardContainer\"> ";
-          echo "<div class=\"cardFront\" style=\"background-image: url(".$url_img." )\"> ";
-          //Change the url nos
-
-          echo " <div class=\"cardInner\"> ";
-          echo " <p>Quote by</p>";
-
-          echo "<span>".$author_name."</span>"; 
-          //Change the name of the author 
-
-          echo "</div> </div> ";        
-          echo "<div class=\"cardBack\"> ";     
-          echo "<div class=\"cardInner\">"; 
-          echo "<p>".$quote."</p> ";  
-          //Change the quote
-
-          echo "</div> </div> </div> </div>"; 
-
-          }//while
-      }//if
-      
-
-    ?>
-
-      </div> <!-- end of cardCols -->
-    </div>
-</div>
 
 <br> <br>
-
-
 
 
 <div id="fixed">
