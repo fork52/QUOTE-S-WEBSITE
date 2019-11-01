cb9637248c67a8f738609d14cfba4c86536f7a69
fork52 <mrunankmistry52@gmail.com>
2019-11-01 10:42:12 +0530

    PHPSPELLCECK ADDED

--- Upload/upload.php
+++ Upload/upload.php
@@ -1,5 +1,4 @@
 <?php include "../includes/sessionStarting.php" ?>
-
 <?php 
 if(isset($_SESSION['upload_msg'])){
     echo "<script>alert('" . $_SESSION['upload_msg']."'); </script>";
@@ -36,7 +35,7 @@ if(isset($_SESSION['upload_msg'])){
   <form method="POST">
     <div>
         <center><label for="Textarea1">Upload Your Quote</label></center><br><br>
-        <textarea class="form-control" maxlength='50' name="quote_area" id="Textarea1" rows="7" placeholder="Enter Quote text"></textarea>
+        <textarea class="form-control" maxlength='100' name="quote_area" id="Textarea1" rows="7" placeholder="Enter Quote text"></textarea>
         <br><br>
         <center><button type="submit" name="submitQuote" class="myButton">Submit</button>
 </center>
@@ -52,8 +51,6 @@ if(isset($_SESSION['upload_msg'])){
   $mySpell->InstallationPath = "../phpspellcheck/";
   $mySpell->Fields = "ALL";
   //echo $mySpell->Activate();
-
-
 ?>
 
 
@@ -62,26 +59,30 @@ if(isset($_SESSION['upload_msg'])){
 <div class="container">
   <div class="errorText">
   <?php 
+
     $quoteText="";
     $errorStatus=False;
     if (isset($_POST['submitQuote']) and $_SERVER["REQUEST_METHOD"] == "POST"){
-      $quoteText = mysqli_real_escape_string($conn,$_POST["quote_area"]);  //Post is a superglobal
-      echo"<br><h1>Your quote :\"".$quoteText."\"<h1>" ;
+      $quoteText3=$_POST["quote_area"];
+      $quoteText=$quoteText2= mysqli_real_escape_string($conn,$_POST["quote_area"]);
+        //Post is a superglobal
+
+      echo"<br><h1>Your quote :\"".stripcslashes($quoteText)."\"<h1>" ;
     }
 
 
-       $r1='/[!@#$%^*()\_=+{}<>]/';  //specialchars
+    $r1='/[!@#$%^*()\/_=+{}<>]/';  //specialchars
 
-        if(preg_match_all($r1,$quoteText)>0){
-             echo"<br><h1>Specialcharacters not allowed.</h1>";
-             $errorStatus=True;
-        }
-        elseif (str_word_count($quoteText)<2) {
-            echo"<br><h1>Minimum two words required.</h1>" ;
-            $errorStatus=True;
-        }
+    if(preg_match_all($r1,$quoteText)>0){
+           echo"<br><h1>Specialcharacters not allowed.</h1>";
+           $errorStatus=True;
+    }
+    elseif(str_word_count($quoteText)<2) {
+          echo"<br><h1>Minimum two words required.</h1>" ;
+          $errorStatus=True;
+    }
+    elseif($errorStatus==False){
 
-        else{
         $sql_query = "SELECT DISTINCT quote_text FROM `quotes`;";
         $result = mysqli_query($conn,$sql_query) ;
         $resultCheck = mysqli_num_rows($result);
@@ -92,18 +93,74 @@ if(isset($_SESSION['upload_msg'])){
 
             $quote_fetched =$row["quote_text"];
 
-            if( strtolower($quoteText) == strtolower($quote_fetched) ){
-              echo "<h1>Quote already exists in the database! We do not support plagiarism.</h1>";
-              $errorStatus=True;
-              break;
+            if( strtolower($quoteText2) == strtolower($quote_fetched) OR 
+                $quoteText3==$quote_fetched OR 
+                strtolower($quoteText3)==strtolower($quote_fetched) OR
+                strtolower($quoteText)==strtolower($quote_fetched) 
+              ){
+                echo "<h1>Quote already exists in the database! We do not support plagiarism.</h1>";
+                $errorStatus=True;
+                break;
             }
             }//while
 
         }//if      
-      }//elseif
+    }//elseif
+
+    elseif($errorStatus==False) {
+        // require "../phpspellcheck/include.php";
+
+        $spellcheckObject = new PHPSpellCheck();
+
+        $spellcheckObject -> LicenceKey="TRIAL";
+
+        $spellcheckObject -> IgnoreAllCaps = false;
+        $spellcheckObject -> IgnoreNumeric = false;
+        $mySpell->CheckGrammar = true;
+        $spellcheckObject -> CaseSensitive = true;
+
+        $spellcheckObject -> DictionaryPath = ("../phpspellcheck/dictionaries/"); 
+        $spellcheckObject -> SuggestionTollerance = 1;
+
+        $spellcheckObject -> LoadDictionary("English (International)") ;
+        $spellcheckObject -> LoadCustomDictionary("custom.txt");
 
 
-      if($errorStatus==False){
+        /*
+
+        $spellcheckObject -> LoadCustomBannedWords("language-rules/banned-words.txt"); 
+        /*
+        You can also add banned words from an array which you could easily populate from an SQL query
+        //$spellcheckObject -> AddBannedWords(array("primary"));
+        */
+
+        $spellcheckObject -> LoadEnforcedCorrections("language-rules/enforced-corrections.txt");
+        $spellcheckObject -> LoadCommonTypos("language-rules/common-mistakes.txt");
+
+        $quoteText=trim($quoteText);
+        $quoteText=stripslashes($quoteText);
+        $words=explode(" ",$quoteText);
+        $last_word = end($words);
+        if(strpos($last_word, '.')){
+          $last_word=substr_replace($last_word ,"",-1);
+          $index = count( $words ) - 1;
+          $words[$index]=$last_word;
+        }
+
+        foreach($words as $word){
+
+            $spelledItRight = $spellcheckObject->SpellCheckWord($word);
+            if(!$spelledItRight){
+              echo "<h1>Please enter correct spelling of words and appropriate words.</h1>";
+              $errorStatus=True;
+              break;
+            }
+        }
+      }
+
+
+
+    if($errorStatus==False){
       //No errors
       $author_name=$_SESSION['user_first']." ".$_SESSION['user_last'];
       //Insert the quote inside in the database using the query as we are good to go
@@ -112,7 +169,7 @@ if(isset($_SESSION['upload_msg'])){
 
       //Redirecting the page to home page for now
         $_SESSION['upload_msg']="Quote Uploaded!";
-        header("Location:../Upload/upload.php");
+        header("Location:./upload.php");
 
     }//end of success if
       
@@ -122,7 +179,7 @@ if(isset($_SESSION['upload_msg'])){
 </div>
 
 
-<br><br><br><br>
+<br><br><br>
 
 
 
